45e54ddd447feff4df048f51a773140907ea44bc
Devpratap-Tomar <devpratap.tomar2005@gmail.com>
2025-02-15 00:15:09 +0530

    few change

--- backend/utils/generateUserTokens.js
+++ backend/utils/generateUserTokens.js
@@ -4,12 +4,12 @@ dotenv.config()
 
 export const generateUserAccessToken=(user)=>{
     return jwt.sign({id:user._id},process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn:'1m'
+        expiresIn:'15m'
     })
 }
 export const generateUserRefreshToken=(user)=>{
     return jwt.sign({id:user._id},process.env.REFRESH_TOKEN_SECRET,{
-        expiresIn:'2m'
+        expiresIn:'1d'
     })
 }
 
