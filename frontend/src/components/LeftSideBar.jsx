9d95c1b29b7dc6c9529eb077e3e2a3ace9796e0b
Devpratap-Tomar <devpratap.tomar2005@gmail.com>
2025-02-27 23:29:56 +0530

    fixed a css issue

--- frontend/src/components/LeftSideBar.jsx
+++ frontend/src/components/LeftSideBar.jsx
@@ -9,7 +9,7 @@ import { setShowCreateModal } from '../slices/showCreateModal'
 const LeftSideBar = () => {
   const dispatch=useDispatch()
   return (
-    <div className="flex flex-col gap-75 w-16 fixed shadow-right min-h-screen top-[2.74rem]">
+    <div className="flex flex-col justify-between w-16 fixed shadow-right h-[calc(100vh-16px)] top-[2.74rem]">
       <div className="top flex flex-col gap-5 p-2">
       <div className="create bg-blue-500 p-3 rounded-lg hover:bg-blue-400" onClick={()=>dispatch(setShowCreateModal(true))}>
       <img className="m-auto w-[26px]" src={Plus} alt="create" />
@@ -25,7 +25,7 @@ const LeftSideBar = () => {
       </Link>
       </div>
       </div>
-      <div className="bottom">
+      <div className="bottom relative bottom-[7%]">
       <div className="settings transition-transform duration-300 ease-in-out hover:rotate-90">
         <Link to={'/settings'}>
       <img className="m-auto w-[30px]" src={Setting} alt="Settings" />
