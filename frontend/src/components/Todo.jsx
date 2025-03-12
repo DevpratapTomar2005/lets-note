26a6a11c72d52a3341b8b4388689c6c702b9b8c3
Devpratap-Tomar <devpratap.tomar2005@gmail.com>
2025-03-12 20:07:41 +0530

    designing todo

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,12 +1,22 @@
-
-
+import verticalDots from '../assets/icons and logos/vertical_dots.svg'
+import clock from '../assets/icons and logos/clock.svg'
 const Todo = () => {
   return (
-    <div className="py-7 px-3 outline-2 w-[300px] my-2 rounded-md" >
+    <div className="pt-7 pb-1 px-3 outline-2 outline-purple-800 w-[300px] my-2 rounded-md" >
+      <div className="flex justify-between items-center">
+
         <div className="flex gap-3">
-        <input type="checkbox" name="taskCheck" id="taskCheck" className="" />
-        <span><h2>Do your task!</h2></span>
+        <input type="checkbox" name="taskCheck" id="taskCheck" className="accent-purple-500 w-[18px]" />
+        <span className="text-purple-600"><h2>Do your task!</h2></span>
         </div>
+        <div className='p-1 hover:bg-gray-100 rounded-full'>
+          <img src={verticalDots} className='w-[25px]' alt="verticle dots" />
+        </div>
+      </div>
+      <div className='mt-1 flex justify-between'>
+        <img src={clock} className='w-[16px] opacity-[0.5] hover:opacity-[1]' alt="clock" />
+        <span className='text-sm text-purple-400'>Due Date:</span>
+      </div>
     </div>
   )
 }
