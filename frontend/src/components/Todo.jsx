6ca0137287ba45db5ae8e82aba55ae2ae7acec39
Devpratap-Tomar <devpratap.tomar2005@gmail.com>
2025-03-13 15:39:21 +0530

    todo design completed and displaying todo on todo page

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,23 +1,37 @@
-import verticalDots from '../assets/icons and logos/vertical_dots.svg'
+import deleteTodo from '../assets/icons and logos/delete.svg'
 import clock from '../assets/icons and logos/clock.svg'
-const Todo = () => {
+import { useState } from 'react'
+const Todo = ({todo}) => {
+const [markComplete,setMarkComplete]=useState(false)
+ const onCheckHandler=()=>{
+ setMarkComplete(!markComplete)
+ }
+ 
   return (
+    
     <div className="pt-7 pb-1 px-3 outline-2 outline-purple-800 w-[300px] my-2 rounded-md" >
       <div className="flex justify-between items-center">
 
-        <div className="flex gap-3">
-        <input type="checkbox" name="taskCheck" id="taskCheck" className="accent-purple-500 w-[18px]" />
-        <span className="text-purple-600"><h2>Do your task!</h2></span>
+        <div className="flex gap-2">
+        <input type="checkbox" name="taskCheck" id="taskCheck" onChange={()=>onCheckHandler()} className="accent-purple-500 w-[18px]" />
+        <span className="text-purple-600"><h2 className={markComplete?'line-through': null}>{todo.title}</h2></span>
         </div>
         <div className='p-1 hover:bg-gray-100 rounded-full'>
-          <img src={verticalDots} className='w-[25px]' alt="verticle dots" />
+          <img src={deleteTodo} className='w-[20px] opacity-[0.8]' alt="verticle dots" />
         </div>
       </div>
-      <div className='mt-1 flex justify-between'>
-        <img src={clock} className='w-[16px] opacity-[0.5] hover:opacity-[1]' alt="clock" />
-        <span className='text-sm text-purple-400'>Due Date:</span>
+      <div className='mt-3 flex justify-between'>
+      {
+            (todo.notifyMe)?( <div className='flex  opacity-[0.5] hover:opacity-[1]'>
+          
+              <img src={clock} className='w-[16px]' alt="clock" />
+              <span className='text-sm text-purple-700 mx-1'>{todo.notificationTime}</span>
+              </div>):(<div></div>)
+          }
+        <span className='text-sm text-purple-400 hover:text-purple-700'>{new Date(todo.dueDate).toLocaleDateString('en-IN')}</span>
       </div>
     </div>
+    
   )
 }
 
--- frontend/src/components/TodosPage.jsx
+++ frontend/src/components/TodosPage.jsx
@@ -4,13 +4,19 @@ import LeftSideBar from './LeftSideBar'
 import Todo from "./Todo.jsx";
 const TodosPage = () => {
   const showCreateModal = useSelector(state => state.showCreateModal.value)
+  const todos=useSelector(state=>state.user.todos)
+ 
   return (
     <div className='flex'>
       <LeftSideBar />
-      <div className="h-[calc(90vh+16px)] relative w-full top-[2.74rem] ml-20">
+      <div className="h-[calc(90vh+16px)] bg-white relative w-full top-[2.74rem] ml-20">
       {showCreateModal && <CreateModal />}
       <div className=" flex flex-wrap gap-3 mx-4 my-7">
-        <Todo/>
+        {
+          todos.map((todo)=>{
+           return <Todo key={todo._id} todo={todo}/>
+          })
+        }
         
       </div>
       </div>
